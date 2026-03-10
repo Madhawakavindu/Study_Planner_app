b5826b9f8d0fdaa72dc91cd74df5617e81aa8ebe
madhawa <madhawakavindu7@gmail.com>
2026-03-10 08:34:08 +0530

    up

---
+++ lib/router/router.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:study_planner/pages/home_page.dart';
+
+class RouterClass {
+  final router = GoRouter(
+    initialLocation: "/",
+    errorPageBuilder: (context, state) {
+      return const MaterialPage(
+        child: Scaffold(body: Center(child: Text("This page is available!"))),
+      );
+    },
+    routes: [
+      //homepage
+      GoRoute(
+        path: "/",
+        name: "home",
+        builder: (context, state) {
+          return HomePage();
+        },
+      ),
+    ],
+  );
+}
