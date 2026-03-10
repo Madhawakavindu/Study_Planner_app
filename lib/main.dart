22e21fffb3c7d59c8d058d5323cf7d34937ff110
madhawa <madhawakavindu7@gmail.com>
2026-03-10 09:17:15 +0530

    u

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:study_planner/router/router.dart';
 
